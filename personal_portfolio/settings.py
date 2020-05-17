a9ada7696059f93b3c1866d8c08bd85dafa08053
Abhay Gangwar <abhaygangwar3@gmail.com>
2020-05-17 12:30:55 +0530

    Added a new pic

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,3 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file present. It seems you are on production.")
